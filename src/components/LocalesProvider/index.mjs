a9cc626f5f0b5c204c00b59ba5f7e6b38e8b3508
eche-unicorn <d.anasteysha@gmail.com>
2023-04-15 20:46:39 +0300

    location video added

---
+++ src/components/LocalesProvider/index.mjs
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import LocalesProviderContext from "./context.mjs";
+
+const LocalesProvider = ({ changeLocale, children, localeNames, messages }) => {
+  return (
+    <LocalesProviderContext.Provider
+      value={{ changeLocale, localeNames, messages }}
+    >
+      {children}
+    </LocalesProviderContext.Provider>
+  );
+};
+
+LocalesProvider.propTypes = {
+  changeLocale: PropTypes.func.isRequired,
+  children: PropTypes.element.isRequired,
+  localeNames: PropTypes.object.isRequired,
+  messages: PropTypes.object.isRequired,
+};
+
+export default LocalesProvider;
