796f2a5425e7bf3bc4fc2821b3a6529bb776d7ee
eche-unicorn <d.anasteysha@gmail.com>
2023-09-24 20:20:10 +0300

    specialists added

---
+++ src/api/about-descr/controllers/about-descr.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * about-descr controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::about-descr.about-descr');
---
+++ src/api/about-descr/services/about-descr.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * about-descr service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::about-descr.about-descr');
